db261e50e9e09da4bfcfc23406c8d6e3c690f9f3
junho <junholim98@gmail.com>
2022-12-10 19:29:06 +0900

    .

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -24,7 +24,7 @@ fi
 
 
 
-nohup java -jar -Duser.timezone=KST -Dserver.port=${TARGET_PORT} /home/ubuntu/var/www/BOB_Server/build/libs/*.jar --spring.profiles.active=prod > /home/user/nohup.out 2>&1 &
+nohup java -jar -Duser.timezone=KST -Dserver.port=${TARGET_PORT} /home/ubuntu/var/www/BOB_Server/build/libs/*.jar --spring.profiles.active=prod > /home/ubuntu/nohup.out 2>&1 &
 #nohup java -jar -Duser.timezone=KST -Dserver.port=8082 /home/ubuntu/var/www/BOB_Server/build/libs/*.jar --spring.profiles.active=prod &
 
 echo "> Now new WAS runs at ${TARGET_PORT}."
